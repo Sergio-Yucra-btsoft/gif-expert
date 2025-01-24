6adde3d9491dc32e06bc60d2e7083639891f52fd
Sergio Yucra <sergio.yucra.btsoft@gmail.com>
2025-01-24 18:03:31 -0400

    first commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,14 @@
+export const GiftExpertApp = () => {
+    return (
+        <>
+            {/* titulo */}
+            <h1>GifExpertApp</h1>
+            
+            {/* input */}
+
+            {/* Listado de gift */}
+
+            {/* Gif Item */}
+        </>
+    )
+}
