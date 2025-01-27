ad7544a934932267579ad89f38cd14218260d9c1
Sergio Yucra <sergio.yucra.btsoft@gmail.com>
2025-01-27 18:08:18 -0400

    v78

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 export const GiftExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Data']);
+
+    const onAddCategory = () => {
+        setCategories([ ...categories, 'new']);
+    }
     return (
         <>
             {/* titulo */}
@@ -7,7 +15,14 @@ export const GiftExpertApp = () => {
             {/* input */}
 
             {/* Listado de gift */}
-
+            <button onClick={ onAddCategory }>Agregar</button>
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={ category }>{category}</li>
+                    })
+                }
+            </ol>
             {/* Gif Item */}
         </>
     )
