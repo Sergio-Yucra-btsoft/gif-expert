2b8042bf90ad566eaeb1ab33a33e3893e3420cd7
Sergio Yucra <sergio.yucra.btsoft@gmail.com>
2025-02-04 17:52:44 -0400

    mostrar titulos de las imagenes

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,5 @@
-export const getGifs = async (category) => {
-  const url =
-    "https://api.giphy.com/v1/gifs/search?api_key=c3B7eWKiIWRI2R9hOJ6i2PmtPaGc1CYh&q=${ category }&limit=20";
+export const getGifs = async ( category ) => {
+  const url =`https://api.giphy.com/v1/gifs/search?api_key=2Y30DFQZ41hLcx4ZVh1gKH5aZXDY2D5q&q=${category}&limit=10`;
   const resp = await fetch(url);
   const { data } = await resp.json();
 
